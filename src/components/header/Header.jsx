2492edc53c7a004ff798ee03ea67dd1c0e480c55
ellinacv <113457842+ellinacv@users.noreply.github.com>
2023-08-08 20:35:20 +0300

    feat: Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,40 @@
 import React from 'react';
 
+import './header.css';
+import { Logo } from '../../svg/Logo';
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-row">
+          <div className="header__logo">
+            <Logo />
+            <span>Fashion</span>
+          </div>
+          <nav className="header__nav">
+            <ul>
+              <li>
+                <a href="#!">Catalogue</a>
+              </li>
+              <li>
+                <a href="#!">Fashion</a>
+              </li>
+              <li>
+                <a href="#!">Favourite</a>
+              </li>
+              <li>
+                <a href="#!">Lifestyle</a>
+              </li>
+              <li>
+                <a href="#!" className="header__nav_btn">
+                  Sign up
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
